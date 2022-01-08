06747782da2a23d7b459881c6e9d14da657a9e74
Godsheritage <Adeoyegodsheritage@gmail.com>
2022-01-08 16:12:11 +0100

    This is my first commit

--- js/main.js
+++ js/main.js
@@ -1,33 +1,34 @@
 const section = document.querySelector('section');
-
+const lists = document.querySelector('.listss');
 const h1 = document.createElement('h1');
+const input = document.createElement('input');
+const ol = document.createElement('ol')
+
+
+
 h1.innerText = "To-Do list";
 h1.classList.add("h-style");
 section.appendChild(h1);
 
-const h2 =document.createElement('h2')
-h2.innerText = "This is my 2nd js Project and its a To-Do list";
-h2.classList.add("h-style");
-section.appendChild(h2);
 
-const input = document.createElement('input')
 input.setAttribute('placeholder', 'To-Do')
 input.setAttribute('type', 'text')
+input.classList.add('form-control')
 section.appendChild(input);
 
-const ul = document.createElement('ul')
-section.appendChild(ul)
+
+lists.appendChild(ol)
 
 // to get the input and add the listener
-const inputs = document.querySelector('input');
-inputs.addEventListener('keypress',function(e){
+input.addEventListener('keypress',function(e){
     if(e.key === 'Enter'){
         const li = document.createElement('li')
         li.setAttribute('id','list-items')
-        ul.appendChild(li)
+        ol.appendChild(li)
         li.innerText = this.value;
         this.value = '';
     }
+   
 })
 
 // const lists = document.querySelector('#list-items');
